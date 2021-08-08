34d4d4f44a8df5463887aca2e3cef2011a55b47c
Nahid Hasan <nahidnub1126@gmail.com>
2021-08-08 13:33:05 +0600

    Trying to create one way server

---
+++ Server_1_Way/src/com/Nahid/company/Client.java
@@ -0,0 +1,21 @@
+package com.Nahid.company;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+	// write your code here
+        try{
+            Socket s=new Socket("localhost",6666);
+            DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+            dos.writeUTF("Hello");
+            dos.flush();
+            dos.close();
+            s.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
--- Server_1_Way/src/com/Nahid/company/Server.java
+++ Server_1_Way/src/com/Nahid/company/Server.java
@@ -1,2 +1,20 @@
-package com.Nahid.company;public class Server {
+package com.Nahid.company;
+
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try{
+            ServerSocket ss=new ServerSocket(6666);
+            Socket s=ss.accept();
+            DataInputStream dis=new DataInputStream(s.getInputStream());
+            String str=dis.readUTF();
+            System.out.println("Msg :"+str);
+            ss.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
