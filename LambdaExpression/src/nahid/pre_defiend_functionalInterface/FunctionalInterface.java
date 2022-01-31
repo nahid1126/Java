6bddfb32489b04b3df1b7e1a453ac341c65ff9ff
Nahid Hasan <nahidnub1126@gmail.com>
2022-02-01 00:49:01 +0600

    learn pre_defiend Functional Interface with apply() andThen() compose() method

---
+++ LambdaExpression/src/nahid/pre_defiend_functionalInterface/FunctionalInterface.java
@@ -0,0 +1,27 @@
+package nahid.pre_defiend_functionalInterface;
+
+import java.util.function.Function;
+
+public class FunctionalInterface {
+    public static void main(String[] args) {
+
+        //for normal function
+        Function<Integer, Integer> f = n -> n * n;
+        System.out.println(f.apply(5));
+
+
+        Function<String, Integer> fu = n -> n.length();
+        System.out.println(fu.apply("Nahid Hasan"));
+
+        //Function chaining
+
+        Function<Integer, Integer> function = i -> i * i;
+        Function<Integer, Integer> functionTwo = i -> i * i * i;
+
+        //andThen first for function then work for functionTwo
+        System.out.println("For andThen method" + function.andThen(functionTwo).apply(2));
+
+        //andThen first for functionTwo then work for function reverse andThen
+        System.out.println(function.compose(functionTwo).apply(5));
+    }
+}
---
+++ LambdaExpression/src/nahid/pre_defiend_functionalInterface/PassingObject.java
@@ -0,0 +1,41 @@
+package nahid.pre_defiend_functionalInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class PassingObject {
+    public static void main(String[] args) {
+        List<Employee> empList = new ArrayList<>();
+        empList.add(new Employee("Nahid Hasan", 20000));
+        empList.add(new Employee("Al-Amin", 25000));
+        empList.add(new Employee("Naimur Rahman", 30000));
+        empList.add(new Employee("Nayeem Bhuiyan", 30000));
+
+        Function<Employee, Integer> function = i -> {
+            int sal = i.salary;
+            if (sal >= 10000 && sal <= 20000) return (sal * 10 / 100);
+            else if (sal > 20000 && sal <= 30000) return (sal * 20 / 100);
+            else if (sal > 30000 && sal <= 40000) return (sal * 30 / 100);
+            else return (sal * 40 / 100);
+        };
+
+        for (Employee emp : empList) {
+            int b = function.apply(emp);
+            System.out.println("Name : " + emp.name + "\nSalary : " + emp.salary +"\nfBonus : " + b);
+        }
+
+
+
+    }
+}
+
+class Employee {
+    String name;
+    int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
