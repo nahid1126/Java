ae7a4dcb156679c4a22e32e157a31a82cc3cccf6
Nahid Hasan <nahidnub1126@gmail.com>
2022-07-02 01:07:14 +0600

    stream are awesome now i'm using stram flatMap

--- Streams/src/nahid/FlatMap/FlatMapDemo.java
+++ Streams/src/nahid/FlatMap/FlatMapDemo.java
@@ -11,9 +11,10 @@ public class FlatMapDemo {
         List<Integer> list = Arrays.asList(1, 2, 3);
         List<Integer> list2 = Arrays.asList(4, 5, 6);
         List<Integer> list3 = Arrays.asList(7, 8, 9);
+        List<Integer> list4 = Arrays.asList(10, 11, 12);
 
         //add into list to list of integer
-        List<List<Integer>> finalList = Arrays.asList(list, list2, list3);
+        List<List<Integer>> finalList = Arrays.asList(list, list2, list3, list4);
         System.out.println(finalList);
 
         //now add into stream to store single list
